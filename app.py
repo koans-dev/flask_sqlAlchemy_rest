ef42c94381507c869250d2a5329f172ba1c2f1cb
Ashish <aashish.bhujel@gmail.com>
2022-07-28 16:01:40 +0545

    Added : Frontend in flask project

--- app.py
+++ app.py
@@ -1,45 +1,53 @@
-from pickle import TRUE
-from unicodedata import name
-from unittest import result
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow 
-import os 
+from flask_marshmallow import Marshmallow
+import os
 
-#Init app 
+#Init app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 #Database
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +os.path.join(basedir,'db.sqlite')
-app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init db
 db = SQLAlchemy(app)
 
-# marshmallow init 
-
+# marshmallow init
 ma = Marshmallow(app)
 class Product(db.Model):
-    id = db.Column (db.Integer, primary_key =True)
-    name = db.Column(db.String(100),unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
     description = db.Column(db.String(200))
     price = db.Column(db.Float)
     qty = db.Column(db.Integer)
 
-    def __init__(self,name,description,price,qty) :
-   
-        self.name = name
-        self.description =description
-        self.price = price
-        self.qty = qty 
+    def __init__(self, name, description, price, qty):
 
-db.create_all()        
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
+
+db.create_all()
+
+
 class ProductSchema(ma.Schema):
-        class Meta:
-            model = Product
-            fields =('id','name','description','price','qty')
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'price', 'qty')
+
 
 product_schema = ProductSchema()
 products_schemas = ProductSchema(many=True)
+
+
+@app.route('/')
+def index():
+    all_products = Product.query.all()
+    return render_template('index.html', products=all_products)
+
 
 @app.route('/product', methods=['POST'])
 def add_product():
@@ -47,13 +55,14 @@ def add_product():
     description = request.json['description']
     price = request.json['price']
     qty = request.json['qty']
-    
-    new_product = Product(name,description,price,qty)
+
+    new_product = Product(name, description, price, qty)
 
     db.session.add(new_product)
     db.session.commit()
 
     return product_schema.jsonify(new_product)
+
 
 @app.route('/product', methods=['GET'])
 def get_products():
@@ -61,13 +70,14 @@ def get_products():
   result = products_schemas.dump(all_products)
   return jsonify(result)
 
-  #Get single product 
+  #Get single product
+
 
 @app.route('/product/<id>', methods=['GET'])
 def product(id):
-  product = Product.get(id)
+  product = Product.query.get(id)
   return product_schema.jsonify(product)
-    
-if __name__ =='__main__':
-    app.run(debug=True)
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
